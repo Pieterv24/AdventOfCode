06815c63d86fdf690a5c47873c5b4e923932d652
Pieter <9167905+Pieterv24@users.noreply.github.com>
2021-12-17 12:15:21 +0100

    finished day 14

--- Day 14/puzzle2.js
+++ Day 14/puzzle2.js
@@ -10,18 +10,19 @@ const sampleFile = 'sampleInput.txt';
  * @param {Object} rules 
  * @returns 
  */
-function addStep(template, rules) {
-    console.log(template);
-    let polymer = [];
+function addStep(pairCounter, rules) {
+    const newPairCounter = {...pairCounter};
+    // Reset counts
+    Object.keys(newPairCounter).forEach(pair => newPairCounter[pair] = 0);
 
-    for (let i = 0; i < template.length - 1; i++) {
-        const pair = template.slice(i, i+2);
-        polymer.push(pair[0]);
-        polymer.push(rules[pair.join('')]);
-    }
-    polymer.push(template[template.length - 1])
+    Object.keys(pairCounter).forEach(pair => {
+        const newPairs = rules[pair];
+        const currentCount = pairCounter[pair];
 
-    return polymer;
+        newPairs.forEach(newPair => newPairCounter[newPair] += currentCount)
+    });
+
+    return newPairCounter;
 }
 
 async function processLineByLine() {
@@ -39,9 +40,10 @@ async function processLineByLine() {
     for await (const line of rl) {
         if (line.includes('->')) {
             const rule = line.split(' -> ');
-            rules[rule[0]] = rule[1];
+            const newPairs = [rule[0][0] + rule[1], rule[1] + rule[0][1]]
+            rules[rule[0]] = newPairs;
         } else if (line.length > 1) {
-            template = line.split('');
+            template = line;
         }
     }
 
@@ -49,17 +51,39 @@ async function processLineByLine() {
     console.log(rules);
     
     const steps = 40;
-    let currentPolymer = template;
+    let currentPolymer = {};
+    // Add Pair counters
+    Object.keys(rules).forEach(pair => currentPolymer[pair] = 0);
+    
+    const templatePairs = template.split('').map((e, i) => i < template.length - 1 ? e + template[i+1] : undefined).filter(e => e !== undefined);
+    templatePairs.forEach(pair => currentPolymer[pair]++);
+
     for (let i = 0; i < steps; i++) {
         currentPolymer = addStep(currentPolymer, rules);
         console.log(currentPolymer);
     }
 
-    const elementCount = currentPolymer.reduce((pv, cv) => (pv[cv] ? pv[cv]++ : pv[cv] = 1, pv), {});
-    const highest = Math.max(...Object.values(elementCount));
-    const lowest = Math.min(...Object.values(elementCount));
+    const countOccurences = {};
+    Object.keys(currentPolymer).forEach((pair, index) => {
+        if (!countOccurences[pair[0]]) {
+            countOccurences[pair[0]] = 0;
+        }
+        countOccurences[pair[0]] += currentPolymer[pair];
+        if (index == Object.keys(currentPolymer).length - 1) {
+            if (!countOccurences[pair[1]]) {
+                countOccurences[pair[1]] = 0;
+            }
+            countOccurences[pair[1]] += currentPolymer[pair];
+        }
+    });
+    // Add 1 count for last polymer node
+    countOccurences[template[template.length - 1]] += 1;
+
+    const highest = Math.max(...Object.values(countOccurences));
+    const lowest = Math.min(...Object.values(countOccurences));
 
     console.log(`The answer is: ${highest - lowest}`);
 }
 
 processLineByLine();
+
