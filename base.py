ff674c70c8f50a0d36090225bd6b8a455dba6a1d
Comon <comon928@gmail.com>
2023-03-20 09:26:26 +0300

    Added file reading

--- base.py
+++ base.py
@@ -1,10 +1,17 @@
 import sys
 from os import path
+import time
 
 if __name__ == "__main__":
     if (len(sys.argv) < 2):
         print(f"Please select file to read")
         exit()
     if (path.exists(sys.argv[1])):
+      
         filePath = sys.argv[1]
-        print(f"Processing {filePath}...")
+        print(f"Processing {filePath}...\n")
+        time.sleep(2)
+        file = open(filePath, 'r')
+        file_content = file.read()
+        print(file_content)
+        f.close()
